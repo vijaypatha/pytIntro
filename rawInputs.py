563d4f4dccebf73b6a693f0c94f807565d63d227
vijaypatha <elderpatha@gmail.com>
2016-11-23 14:44:13 -0700

    first commit

---
+++ rawInputs.py
@@ -0,0 +1,5 @@
+age = raw_input("What is your age?")
+name = raw_input("what is your name?")
+height = raw_input("what is your height?")
+home = raw_input("where do you live?")
+print "so you are %r old with name %r of height %r living in %r " % (age,name,height,home)
---
+++ variables.py
@@ -0,0 +1,19 @@
+cars = 100
+space_in_a_car = 4
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+print "There are", cars , "cars avilable"
+print "There are only", drivers , "drivers avilable"
+print "each car carries", space_in_a_car, "passengers per car"
+print "hence there is space for", drivers * space_in_a_car, "passengers"
+print "there are %s cars" % cars
+print "there are only %d drivers" % drivers
+print "mary had a little %s" % 'lamb'
+print "Its fleece was white as %s" % 'snow'
+print "#"*50
+
+end1 = "V"
+end2 = "J"
+Rats = "patha"
+print end1 + end2, Rats
